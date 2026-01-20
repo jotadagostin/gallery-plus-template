080b9d85ac8bc9f27926e8e35995afec49d83968
João Paulo Dagostin <dagostinjota@gmail.com>
2026-01-20 11:18:50 -0300

    new component and function

---
+++ src/contexts/albums/components/albums-list-selectable.tsx
@@ -0,0 +1,62 @@
+import Divider from "../../../components/divider";
+import InputCheckbox from "../../../components/input-checkbox";
+import Skeleton from "../../../components/skeleton";
+import Text from "../../../components/text";
+import type { Photo } from "../../photos/models/photo";
+import type { Album } from "../models/album";
+
+interface AlbumListSelectableProps {
+  loading?: boolean;
+  albums: Album[];
+  photo: Photo;
+}
+
+export default function AlbumListSelectable({
+  albums,
+  photo,
+  loading,
+}: AlbumListSelectableProps) {
+  function isChecked(albumId: string) {
+    return photo?.albums?.some((album) => album.id === albumId);
+  }
+
+  function handlePhotoAlbums(albumId: string) {
+    let albumsIds = [];
+
+    if (isChecked(albumId)) {
+      albumsIds = photo.albums
+        .filter((album) => album.id !== albumId)
+        .map((album) => album.id);
+    } else {
+      albumsIds = [...photo.albums.map((album) => album.id), albumId];
+    }
+
+    console.log("those are the albums for the backend", albumsIds);
+  }
+  return (
+    <ul className="flex flex-col gap-4">
+      {!loading &&
+        albums?.length > 0 &&
+        albums.map((album, index) => (
+          <li key={album.id}>
+            <div className="flex items-center justify-between gap-1">
+              <Text variant="paragraph-large" className="truncate">
+                {album.title}
+              </Text>
+              <InputCheckbox
+                defaultChecked={isChecked(album.id)}
+                onClick={() => handlePhotoAlbums(album.id)}
+              />
+            </div>
+            {index !== albums.length - 1 && <Divider className="mt-4" />}
+          </li>
+        ))}
+      {loading &&
+        Array.from({ length: 5 }).map((_, index) => (
+          <li key={`albums-list-${index}`}>
+            <Skeleton className="h-[2.5rem]" />
+          </li>
+        ))}
+    </ul>
+  );
+}
--- src/pages/pages-photo-details.tsx
+++ src/pages/pages-photo-details.tsx
@@ -3,6 +3,7 @@ import Container from "../components/container";
 import ImagePreview from "../components/image-preview";
 import Skeleton from "../components/skeleton";
 import Text from "../components/text";
+import AlbumListSelectable from "../contexts/albums/components/albums-list-selectable";
 import PhotosNavigator from "../contexts/photos/models/components/photos-navigator";
 import type { Photo } from "../contexts/photos/models/photo";
 
@@ -22,7 +23,9 @@ export default function PagePhotoDetails() {
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
-          <Text variant="heading-large">{photo?.title}</Text>
+          <Text as="h2" variant="heading-large">
+            {photo?.title}
+          </Text>
         ) : (
           <Skeleton className="w-48 h-8" />
         )}
@@ -30,7 +33,7 @@ export default function PagePhotoDetails() {
         <PhotosNavigator loading={isLoadingPhoto} />
       </header>
 
-      <div className="grid grid-cols-[21rem] gap-24">
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
         <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
@@ -39,14 +42,28 @@ export default function PagePhotoDetails() {
               imageClassName="h-[21rem]"
             />
           ) : (
-            <Skeleton className="h-[21rem]" />
+            <Skeleton className="h-84" />
           )}
-
           {!isLoadingPhoto ? (
             <Button variant="destructive">Excluir</Button>
           ) : (
             <Skeleton className="w-20 h-10" />
           )}
+        </div>
+
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-7">
+            Albums
+          </Text>
+          <AlbumListSelectable
+            photo={photo}
+            albums={[
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ]}
+            loading={isLoadingPhoto}
+          />
         </div>
       </div>
     </Container>
